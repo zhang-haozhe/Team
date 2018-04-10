ff64b6b0896ea9e38d76d2fe6d3575ef39c4ae95
Idunknodewae <32111115+rrrrr4788@users.noreply.github.com>
2018-04-10 13:19:42 -0400

    Print and Load

--- Header.h
+++ Header.h
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include <fstream>
+
 
 using namespace std;
 
@@ -12,19 +14,22 @@ void sort(int* userNum);
 void previous(int* userNum);
 void search(int* userNum);
 
-class Node{
+class Node {
 public:
-    Node* next;
-    string value;
+	Node * next;
+	string value;
 };
 
 class List {
 private:
-  int size;
-  Node* head;
+	int size;
+	Node* head;
 public:
-    List();
-    void addTerm(string term);
-    int deleteTerm(string term);
+	List();
+	void addTerm(string term);
+	int deleteTerm(string term);
 	void deleteTermDetails(int index);
+	void load(string fileName);
+	void print();
 };
+#pragma once
