22aff227fd03aab729c37f49b20fd340b3b7e216
Dan Nhi <32380994+hoangdannhi@users.noreply.github.com>
2018-04-19 19:01:08 -0400

    Update header.h

--- final/header.h
+++ final/header.h
@@ -1,1 +1,57 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
 
+void userMenu(int*userNum);
+
+void search(int*userNum);
+void searchByCh(int chIn);
+
+void sort(int*userNum);
+void sortSec(int numSec);
+void sortChp(int numChp);
+void sortEntire(int sub);
+
+void view(int*userNum);
+void viewBySection();
+void viewByCh();
+void viewSelCh();
+void viewSelSec();
+
+void previous();
+
+class Node {
+public:
+	Node * next;
+	string value;
+};
+
+class List {
+private:
+	int size;
+	Node* head;
+public:
+	List();
+	Node*getHead();
+	int getSize();
+	void addTerm(string term);
+	int deleteTerm(string term);
+	void deleteTermDetails(int index);
+	void load(string fileName);
+	bool contains(string term);
+	void print();
+
+	//bubblesort methods
+	void insertAtBeginning(Node**start);
+	void bubbleSort(Node*start);
+	void swap(Node*a, Node*b);
+
+	//search
+	void printSearchDef(string term);
+	string strToUpper(string str);
+	string removeWhiteSpace(string term);
+
+	void printMemory();
+};
+#pragma once
