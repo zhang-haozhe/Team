1c9d67e402fa9eeaed62c7737daa72323c760648
Dan Nhi <32380994+hoangdannhi@users.noreply.github.com>
2018-04-19 03:55:24 -0400

    Update 000Header.h

--- 000Header.h
+++ 000Header.h
@@ -8,14 +8,15 @@ using namespace std;
 
 void creditList(int* userNum);
 void userMenu(int* userNum);
-void addition(int* userNum);
-void deletion(int* userNum);
 void sort(int* userNum);
 void previous(int* userNum);
 void search(int* userNum);
-void table(int* userNum);
-void sortAndPrint(int* userNum);
+void toc(int* userNum);
 
+void view(int* userNum);
+void viewBySection();
+void viewByCh();
+void viewSelCh();
 
 class Node {
 public:
@@ -35,43 +36,25 @@ public:
 	int deleteTerm(string term);
 	void deleteTermDetails(int index);
 	void load(string fileName);
-	bool checkexistence(string pagename);
+	bool contains(string term);
 	void print();
 
 	//bubblesort methods
 	void insertAtBeginning(Node**start);
 	void bubbleSort(Node*start);
 	void swap(Node*a, Node*b);
-	
+
 	//search
 	bool search(Node*head, string term);
 	void printSearch(string term);
 	void printSearchDef(string term);
 	string strToUpper(string str);
-	
+	string removeWhiteSpace(string term);
+
+	void printMemory();
+	void printSection(string sec);
 };
 
-//stack implementation
-struct node
-{
-	string data;
-	struct node *next;
-};
 
-class stack
-{
-	struct node *top;
-public:
-	stack() {
-		struct node *top = NULL;
-	}
-	void push(string value);
-	void pop();
-	void display();
-	bool empty();
-	string topOfStack();
-	string checkTop();
-	bool checkStack(string value);
-};
 
 #pragma once
